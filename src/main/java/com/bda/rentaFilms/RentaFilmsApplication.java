0a6d251816ff9626fc1d968f91aabfcc660ddd61
Antonio Villasante Hilares <hilares33v@gmail.com>
2023-10-18 16:22:01 -0300

    first commit

---
+++ src/main/java/com/bda/rentaFilms/RentaFilmsApplication.java
@@ -0,0 +1,13 @@
+package com.bda.rentaFilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaFilmsApplication.class, args);
+	}
+
+}
